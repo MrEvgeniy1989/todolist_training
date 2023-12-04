aa7ec9cea81cfeb0a29ecb0510d11d8fbe5602cc
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-12-04 18:57:19 +0300

    dev Начальный туду с CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
-function App() {
+export type FilterType = 'all' | 'active' | 'completed'
+
+type TodolistType = {
+    id: string
+    todoTitle: string
+    filter: FilterType
+}
+
+export const App = () => {
+
+    const todolistId1 = crypto.randomUUID()
+    const todolistId2 = crypto.randomUUID()
+
+    const [todolists, setTodolists] = useState<TodolistType[]>([
+        {id: todolistId1, todoTitle: "Что изучить", filter: 'all'},
+        {id: todolistId2, todoTitle: "Что купить", filter: 'all'},
+    ])
+    const [tasks, setTasks] = useState({
+        [todolistId1]: [
+            {id: crypto.randomUUID(), taskTitle: "HTML", isDone: true},
+            {id: crypto.randomUUID(), taskTitle: "CSS", isDone: true},
+            {id: crypto.randomUUID(), taskTitle: "JS", isDone: true},
+            {id: crypto.randomUUID(), taskTitle: "React", isDone: false},
+            {id: crypto.randomUUID(), taskTitle: "Redux", isDone: false}
+        ],
+        [todolistId2]: [
+            {id: crypto.randomUUID(), taskTitle: "Milk", isDone: true},
+            {id: crypto.randomUUID(), taskTitle: "Chocolate", isDone: false},
+            {id: crypto.randomUUID(), taskTitle: "Bread", isDone: true},
+            {id: crypto.randomUUID(), taskTitle: "Butter", isDone: true},
+            {id: crypto.randomUUID(), taskTitle: "Banana", isDone: false}
+        ]
+    })
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist/>
         </div>
     );
 }
-
-export default App;
